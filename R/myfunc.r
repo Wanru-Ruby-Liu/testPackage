9ce4308e25bba455f511000cc2e0b173f3cfe173
Wanru-Ruby-Liu <wanru_liu@brown.edu>
2025-03-14 00:40:07 -0400

    Updated two more functions

--- R/myfunc.r
+++ R/myfunc.r
@@ -12,3 +12,43 @@ myfunc <- function(a, b) {
   result <- a * b + a
   return(result)
 }
+
+#' Estimate Beta for Linear Regression
+#'
+#' @name estimate_beta
+#' @param y A numerical vector of response values.
+#' @param X A numerical matrix of predictor values.
+#' @return A numerical vector of estimated beta coefficients.
+#' @export
+#'
+#' @examples
+#' y <- c(1, 2, 3, 4, 5)
+#' X <- matrix(c(1, 1, 1, 1, 1, 1, 2, 3, 4, 5), ncol = 2)
+#' estimate_beta(y, X)
+estimate_beta <- function(y, X) {
+  beta_hat <- solve(t(X) %*% X) %*% t(X) %*% y
+  return(beta_hat)
+}
+
+
+#' Custom ggplot2 Theme
+#'
+#' @name my_theme
+#' @return A ggplot2 theme object.
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(mtcars, aes(x = mpg, y = hp)) +
+#'   geom_point() +
+#'   my_theme()
+my_theme <- function() {
+  ggplot2::theme_minimal() +
+    ggplot2::theme(
+      text = ggplot2::element_text(family = "Helvetica", size = 14, color = "black"),
+      panel.grid.major = ggplot2::element_line(color = "gray80"),
+      panel.grid.minor = ggplot2::element_blank(),
+      axis.title = ggplot2::element_text(face = "bold"),
+      plot.title = ggplot2::element_text(hjust = 0.5, face = "bold", size = 16)
+    )
+}
